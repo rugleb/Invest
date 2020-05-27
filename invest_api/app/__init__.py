a2524cc962854dcc7e49a55000ab348ab1776d7d
gkarpushkin <rugleb@gmail.com>
2020-05-27 11:49:10 +0300

    moved Company schema to public API

--- invest_api/app/__init__.py
+++ invest_api/app/__init__.py
@@ -1,8 +1,9 @@
 from .factory import create_app, create_tcp_server
-from .models import Company
+from .models import Company, CompanySchema
 
 __all__ = (
     "create_app",
     "create_tcp_server",
     "Company",
+    "CompanySchema",
 )
